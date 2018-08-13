ebcb8ee5c09d1007db059ec4a9403ebff06fea78
Mo'men Soliman <moumensoliman@gmail.com>
2018-08-14 00:05:04 +0200

    update home src image and style

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,47 @@
 <template>
   <div>
-    <h1>Home</h1>
-    <p>Here is some information. This is the home page</p>
+    <div class="full-logos-items">
+      <div class="item" v-for="post in posts" :key="post.title">
+        <router-link class="nav-link" :to="`${post.link}`" exact>
+          <div v-if="post.img != 'notfound'">
+            <b-img-lazy :src="require(`../assets/images/items/${post.img}`)"/>
+          </div>
+          <div v-else>
+            <b-img-lazy :src="`https://lightwidget.com/widgets/empty-photo.jpg`" width="100"/>
+          </div>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import item from '../storedData/items.json'
+
 export default {
-  name: 'Home' // this is the name of the component
+  name: 'Home', // this is the name of the component
+  data () {
+    return {
+      posts: item
+    }
+  }
 }
 </script>
-<style>
+
+<style lang="scss" scoped>
+.full-logos-items {
+  padding: 50px 0;
+  display: flex;
+  justify-content: space-around;
+  .item {
+    margin: auto;
+    img {
+      width: 100px;
+      -webkit-filter: grayscale(100%);
+      &:hover {
+        -webkit-filter: grayscale(0);
+      }
+    }
+  }
+}
 </style>
