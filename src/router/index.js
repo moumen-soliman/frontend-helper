fda579fd074eab21b648a679adf9db0b0b4dbd79
Mo'men Soliman <moumensoliman@gmail.com>
2018-08-13 02:45:40 +0200

    Add history mode and routes for pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import Home from '../components/Home'
+import About from '../components/About'
+import Contact from '../components/Contact'
+import NotFound from '../components/NotFound'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
     }
   ]
 })
