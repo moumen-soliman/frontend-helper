f5056c4ba245985dd922c06b967befab33cc6300
Mo'men Soliman <moumensoliman@gmail.com>
2018-08-13 02:43:53 +0200

    Add navigation bar and make it default in all pages

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <div id="app">
+    <Navigation></Navigation>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navigation from './components/Navigation'
 export default {
-  name: 'App'
+  name: 'app',
+  components: {
+    'Navigation': Navigation
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Ubuntu');
+
+body {
+  background: #f7f8f9,
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Ubuntu', sans-serif;
+  color: #1e90ff;
 }
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <b-container fluid>
+      <b-navbar cols="12" type="dark" variant="blue" toggleable>
+        <b-navbar-toggle target="nav_dropdown_collapse"></b-navbar-toggle>
+        <b-collapse is-nav id="nav_dropdown_collapse">
+            <b-navbar-brand href="/"><span>&lt;</span><b>Frontend</b>Helper<span>/&gt;</span></b-navbar-brand>
+            <!-- Navbar dropdowns -->
+            <b-navbar-nav class="ml-auto">
+              <router-link class="spacing nav-link"  v-for="routes in links"
+              v-bind:key="routes.id"
+              :to="`${routes.page}`" exact>{{routes.text}}</router-link>
+            </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  data () {
+    return {
+      links: [
+        {
+          id: 1,
+          text: 'Home',
+          page: '/'
+        },
+        {
+          id: 2,
+          text: 'About',
+          page: '/About'
+        },
+        {
+          id: 3,
+          text: 'Contact',
+          page: '/Contact'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.spacing {
+  color: rgba(0,0,0,.5) !important;
+}
+.router-link-active {
+  color: rgb(0,0,0) !important;
+  font-weight: 600;
+}
+.container-fluid {
+  padding: 0;
+}
+.bg-blue {
+  background: white;
+}
+.navbar-brand {
+  color: rgba(0, 0, 0, 0.8);
+  &:hover {
+    color: black;
+  }
+  span {
+    color: rgba(0, 0, 0, 0.4);
+  }
+}
+.navbar-dark .navbar-toggler-icon {
+
+}
+</style>
