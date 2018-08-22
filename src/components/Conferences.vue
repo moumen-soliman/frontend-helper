4c1bd3d3b50a69521a14a493b1e0c5ef4963acb5
Mo'men Soliman <moumensoliman@gmail.com>
2018-08-22 22:40:05 +0200

    add months in data

--- src/components/Conferences.vue
+++ src/components/Conferences.vue
@@ -13,7 +13,46 @@ export default {
   name: 'Conferences',
   data () {
     return {
-      conf: conf
+      conf: conf,
+      months: [{
+        month: 'january',
+      },
+      {
+        month: 'february',
+      },
+      {
+        month: 'march',
+      },
+      {
+        month: 'april',
+      },
+      {
+        month: 'may',
+      },
+      {
+        month: 'june',
+      },
+      {
+        month: 'july',
+      },
+      {
+        month: 'august',
+      },
+      {
+        month: 'setember',
+      },
+      {
+        month: 'june',
+      },
+      {
+        month: 'october',
+      },
+      {
+        month: 'november',
+      },
+      {
+        month: 'december',
+      }]
     }
   },
   computed: {
