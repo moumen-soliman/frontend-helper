b4abf1ac1deafab7de09e10657e3bb2a74049608
Mo'men Soliman <moumensoliman@gmail.com>
2018-08-24 23:48:37 +0200

    Add about message

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <h1>{{info}}</h1>
+  <div class="text-center">
+    <p>There is big issue in Front-end field (or Software engineer field), there is no one place that collects all you need, from news and GitHub repos and articles, we all face that problem there is no one source, so I try to create this project that helps developers to check all resources from one place, mainly for front-end developers.</p>
+    <p>Actually this project now is in beta version, Data based on JSON and in clear files, if you are a junior or mid-level or senior or expert you can add your resources and help other developers by add it on JSON file, it's built to be easy in contribute for all developers, You can also add features we still need search and sure resources in every item in homepage</p>
+    <strong>Please start to contribute by resources or features <a href="Please start to contribute by resources or features">Github Repo</a></strong>
   </div>
 </template>
 
@@ -9,10 +11,13 @@ export default {
   name: 'About', // this is the name of the component
   data () {
     return {
-      info: 'About page'
     }
   },
 }
 </script>
 <style lang="scss" scoped>
+div {
+  color: #000;
+  margin-top: 20vh;
+}
 </style>
