962be5552233bf8f2c0af557980f4fe5694d7184
Mo'men Soliman <moumensoliman@gmail.com>
2018-08-26 19:47:26 +0200

    fix href url issue in About page

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,7 +2,7 @@
   <div class="text-center">
     <p>There is big issue in Front-end field (or Software engineer field), there is no one place that collects all you need, from news and GitHub repos and articles, we all face that problem there is no one source, so I try to create this project that helps developers to check all resources from one place, mainly for front-end developers.</p>
     <p>Actually this project now is in beta version, Data based on JSON and in clear files, if you are a junior or mid-level or senior or expert you can add your resources and help other developers by add it on JSON file, it's built to be easy in contribute for all developers, You can also add features we still need search and sure resources in every item in homepage</p>
-    <strong>Please start to contribute by resources or features <a href="Please start to contribute by resources or features">Github Repo</a></strong>
+    <strong>Please start to contribute by resources or features <a href="https://github.com/moumen-soliman/frontend-helper" target="_blank">Github Repo</a></strong>
   </div>
 </template>
 
