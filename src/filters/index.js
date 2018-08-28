30c8bc6de14ee8c1808bf1926c052d7406a7cafd
Mo'men Soliman <moumensoliman@gmail.com>
2018-08-28 17:47:47 +0200

    fix linting and remove unused functions from filter

--- src/filters/index.js
+++ src/filters/index.js
@@ -20,22 +20,3 @@ export function unescape (value) {
   textArea.innerHTML = value
   return textArea.value
 }
-
-// Filter that takes an epoch based UTC date and creates date time value.
-export function timeAgo (time) {
-  const between = Date.now() / 1000 - Number(time)
-  if (between < 3600) {
-    return pluralize(~~(between / 60), ' minute')
-  } else if (between < 86400) {
-    return pluralize(~~(between / 3600), ' hour')
-  } else {
-    return pluralize(~~(between / 86400), ' day')
-  }
-}
-
-function pluralize (time, label) {
-  if (time === 1) {
-    return time + label
-  }
-  return time + label + 's'
-}
