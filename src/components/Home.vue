38b56d6f6c6c0fc0d9f343cf549784526acda1fd
Mo'men Soliman <moumensoliman@gmail.com>
2018-11-10 22:09:51 +0200

    fix lazy loading image issue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,10 +5,10 @@
       <b-col class="item" xs="12" sm="6" md="3" lg="2" v-for="post in filteredPosts" :key="post.title">
         <router-link class="nav-link" :to="`/frontend-helper/item?page=${post.link[0]}`" exact>
           <div v-if="post.img != 'notfound'">
-            <b-img-lazy :src="require(`../assets/images/items/${post.img}`)" />
+            <img :src="require(`../assets/images/items/${post.img}`)" />
           </div>
           <div v-else>
-            <b-img-lazy :src="`https://lightwidget.com/widgets/empty-photo.jpg`" width="100" />
+            <img :src="`https://lightwidget.com/widgets/empty-photo.jpg`" width="100" />
           </div>
         </router-link>
       </b-col>
